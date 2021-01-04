3f5595752dc39a7d38ccd74f0b58e1d3c531f2a3
mosh <mosh.ohayo@gmail.com>
2021-01-05 04:11:23 +0900

    Add link usage in doxygen comment

--- src/greet/greet.h
+++ src/greet/greet.h
@@ -1,19 +1,20 @@
 /**
  * @file greet.h
  * @brief Various greeting functions.
- *
- * Greetings are important.
+ * @details Greetings are important.
  */
 
 /**
  * @brief Greets.
+ * @see greet_n
  */
 void greet(void);
 
 /**
  * @brief Greets `n` times and returns the number charactors in greetings.
- * @param[in] n The number of greetings.
- * @return The total number of charactors in greetings.
  * @details Greetings are important.
+ * @param n The number of greetings.
+ * @return The total number of charactors in greetings.
+ * @see greet
  */
 int greet_n(int n);
--- src/helloworld.c
+++ src/helloworld.c
@@ -1,5 +1,9 @@
 #include "greet/greet.h"
 
+/**
+ * @brief Starting point.
+ * @details Internally calls greet_n(int)
+ */
 int main()
 {
     greet_n(2);
